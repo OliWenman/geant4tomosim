de24d965a6bf2f158ea0559a0d0bc4bf94023b8e
Wenman <xol73553@ws359.diamond.ac.uk>
2018-08-23 15:27:30 +0100

    Seperated Data class into .hh and .cc file

--- include/Data.hh
+++ include/Data.hh
@@ -1,6 +1,7 @@
 #ifndef Data_h
 #define Data_h 1
 
+#include "globals.hh"
 #include <iostream>
 #include <vector>
 #include <math.h>       // Used to access the floor command floor 
@@ -9,40 +10,8 @@
 class Data
 {
 	public:
-		Data()//G4int Nrow, G4int Ncolumn, G4int Nbins)
-		{
-
-			G4cout << G4endl << "Data has been created " << G4endl;
-			/*//Saves the rows and columns inputted using the Set methods
-			SetNumberRows(Nrow);
-			SetNumberColumns(Ncolumn);
-			SetNumberBins(Nbins);
-		
-			G4int EnergyColumns = GetNumberRows() * GetNumberColumns();
-
-			//Creates a 2D vector when the object is created as big as the columns and rows inputted
-			std::vector<std::vector<G4int> > iHitDataMatrix;
-			iHitDataMatrix.resize(Nrow, std::vector<G4int>(Ncolumn));
-			
-			//Creates a 2D vector when the object is created as big as the columns and rows inputted
-			std::vector<std::vector<G4int> > iEnergyMatrix;
-			iEnergyMatrix.resize(bins, std::vector<G4int>(EnergyColumns));
-			
-			//Saves the matrix using the set method			
-			SetHitData(iHitDataMatrix);
-			SetEnergyData(iEnergyMatrix);
-
-			SetNumberOfTotalHits(0);*/
-
-
-		}
-
-		//Data() {G4cout << G4endl << "Data has been created in RunAction" << G4endl;}
-		
-		~Data()
-		{
-			G4cout << G4endl << "Data class has been deleted "<< G4endl;		
-		};
+		Data();
+		virtual ~Data();
 		
 		//Get Methods
 		std::vector <std::vector<int> > GetHitData(){ return HitDataMatrix;}
@@ -74,106 +43,11 @@ class Data
 		G4int Quotient(G4int dividend, G4int divisor ) {return dividend / divisor;}
 		G4int Remainder(G4int dividend, G4int divisor) {return dividend % divisor;}
 
-		void SetUpMatricies(G4int Nrow, G4int Ncolumn, G4int Nbins)
-		{
-			//Saves the rows and columns inputted using the Set methods
-			SetNumberRows(Nrow);
-			SetNumberColumns(Ncolumn);
-			SetNumberBins(Nbins);
-		
-			G4int EnergyColumns = GetNumberRows() * GetNumberColumns();
-
-			//Creates a 2D vector when the object is created as big as the columns and rows inputted
-			std::vector<std::vector<G4int> > iHitDataMatrix;
-			iHitDataMatrix.resize(Nrow, std::vector<G4int>(Ncolumn));
-			
-			//Creates a 2D vector when the object is created as big as the columns and rows inputted
-			std::vector<std::vector<G4int> > iEnergyMatrix;
-			iEnergyMatrix.resize(bins, std::vector<G4int>(EnergyColumns));
-			
-			//Saves the matrix using the set method			
-			SetHitData(iHitDataMatrix);
-			SetEnergyData(iEnergyMatrix);
-
-			SetNumberOfTotalHits(0);
-		}
-
-		void SaveHitData(G4int DetectorNumber)
-		{
-			//Finds the number of columns the matrix has
-			G4int column = GetNumberColumns();
-			
-			//Finds the coordinates of the detector inputted for the matrix
-			G4int x = Quotient(DetectorNumber, column);
-			G4int y = Remainder(DetectorNumber, column);
-
-			//+1 for a hit
-			++HitDataMatrix[x][y]; 
-
-			//Keeps track of the total number of hits
-			SetNumberOfTotalHits(GetNumberOfTotalHits() + 1);
-		}
-		
-		void PrintHitData()
-		{
-			//Find each needed value using the Get functions
-			std::vector <std::vector<G4int> > HitData = GetHitData();
-			G4int Nrows = GetNumberRows();
-			G4int Ncolumns = GetNumberColumns();
-			
-			G4cout << G4endl << "The updated matrix of hits " << G4endl;
-			
-			//Prints out the matrix
-			for(G4int x = 0; x < Nrows; x++)  
-    			{
-    				for(G4int y = 0; y < Ncolumns; y++)  
-        			{	
-					G4cout << HitData[x][y] << " ";
-        			}
-    				G4cout << G4endl;  
-    			}
-		}
-
-		void SaveEnergyData(G4int DetectorNumber, G4double edep)
-		{
-			G4double MaxEnergy = 30*keV;
-
-			SetMaxEnergy(MaxEnergy);
-
-			G4double BinSize = MaxEnergy/GetNumberBins();
-			
-			G4int BinNumber = floor(edep/BinSize);
-
-			++EnergyMatrix[BinNumber][DetectorNumber];
-		}
-		
-		void PrintEnergyData()
-		{
-			std::vector <std::vector<G4int> > EnergyData = GetEnergyData();
-			G4cout << G4endl << "The updated matrix of energy " << G4endl;
-
-			G4cout << "            ";
-			
-			for (G4int x = 0 ; x < GetNumberBins(); x++)
-			{
-				G4cout << (GetMaxEnergy()/GetNumberBins()) * (x+1) << " ";
-			}
-			G4cout << G4endl;
-
-			//Prints out the matrix
-			for(G4int x = 0 ; x < GetNumberRows()*GetNumberColumns(); x++)  
-    			{
-    				for( G4int y = 0 ; y < GetNumberBins(); y++)  
-        			{	
-					if (y == 0)
-					{
-						G4cout << "Detector " << x << ": ";
-					}
-					G4cout << EnergyData[y][x] << " ";
-        			}
-    				G4cout << G4endl;  
-    			}
-		}
+		virtual void SetUpMatricies(G4int Nrow, G4int Ncolumn, G4int Nbins);
+		virtual void SaveHitData(G4int DetectorNumber);
+		virtual void PrintHitData();
+		virtual void SaveEnergyData(G4int DetectorNumber, G4double edep);
+		virtual void PrintEnergyData();
 
 	private:	
 		//Data members
---
+++ src/Data.cc
@@ -0,0 +1,110 @@
+#include "Data.hh"
+
+
+Data::Data(){G4cout << G4endl << "Data class has been created "<< G4endl;}
+
+Data::~Data(){G4cout << G4endl << "Data class has been deleted "<< G4endl;}
+
+void Data::SetUpMatricies(G4int Nrow, G4int Ncolumn, G4int Nbins)
+{
+	//Saves the rows and columns inputted using the Set methods
+	SetNumberRows(Nrow);
+	SetNumberColumns(Ncolumn);
+	SetNumberBins(Nbins);
+		
+	G4int EnergyColumns = GetNumberRows() * GetNumberColumns();
+
+	//Creates a 2D vector when the object is created as big as the columns and rows inputted
+	std::vector<std::vector<G4int> > iHitDataMatrix;
+	iHitDataMatrix.resize(Nrow, std::vector<G4int>(Ncolumn));
+			
+	//Creates a 2D vector when the object is created as big as the columns and rows inputted
+	std::vector<std::vector<G4int> > iEnergyMatrix;
+	iEnergyMatrix.resize(bins, std::vector<G4int>(EnergyColumns));
+			
+	//Saves the matrix using the set method			
+	SetHitData(iHitDataMatrix);
+	SetEnergyData(iEnergyMatrix);
+
+	SetNumberOfTotalHits(0);
+}
+
+void Data::SaveHitData(G4int DetectorNumber)
+{
+	//Finds the number of columns the matrix has
+	G4int column = GetNumberColumns();
+			
+	//Finds the coordinates of the detector inputted for the matrix
+	G4int x = Quotient(DetectorNumber, column);
+	G4int y = Remainder(DetectorNumber, column);
+
+	//+1 for a hit
+	++HitDataMatrix[x][y]; 
+
+	//Keeps track of the total number of hits
+	SetNumberOfTotalHits(GetNumberOfTotalHits() + 1);
+}
+
+void Data::PrintHitData()
+{
+	//Find each needed value using the Get functions
+	std::vector <std::vector<G4int> > HitData = GetHitData();
+	G4int Nrows = GetNumberRows();
+	G4int Ncolumns = GetNumberColumns();
+			
+	G4cout << G4endl << "The updated matrix of hits " << G4endl;
+			
+	//Prints out the matrix
+	for(G4int x = 0; x < Nrows; x++)  
+    	{
+    		for(G4int y = 0; y < Ncolumns; y++)  
+        	{	
+			G4cout << HitData[x][y] << " ";
+        	}
+    		G4cout << G4endl;  
+    	}
+}
+
+void Data::SaveEnergyData(G4int DetectorNumber, G4double edep)
+{
+	G4double MaxEnergy = 30*keV;
+
+	SetMaxEnergy(MaxEnergy);
+
+	G4double BinSize = MaxEnergy/GetNumberBins();
+			
+	G4int BinNumber = floor(edep/BinSize);
+
+	++EnergyMatrix[BinNumber][DetectorNumber];
+}
+
+void Data::PrintEnergyData()
+{
+	std::vector <std::vector<G4int> > EnergyData = GetEnergyData();
+	G4cout << G4endl << "The updated matrix of energy " << G4endl;
+
+	G4cout << "            ";
+			
+	for (G4int x = 0 ; x < GetNumberBins(); x++)
+	{
+		G4cout << (GetMaxEnergy()/GetNumberBins()) * (x+1) << " ";
+	}
+	G4cout << G4endl;
+
+	//Prints out the matrix
+	for(G4int x = 0 ; x < GetNumberRows()*GetNumberColumns(); x++)  
+    	{
+    		for( G4int y = 0 ; y < GetNumberBins(); y++)  
+        	{	
+			if (y == 0)
+			{
+				G4cout << "Detector " << x << ": ";
+			}
+			G4cout << EnergyData[y][x] << " ";
+        	}
+    		G4cout << G4endl;  
+    	}
+}
+
+
+
