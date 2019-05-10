10915aa8a8189c9dd6d160eb9d98bc202e6c5926
Wenman <xol73553@ws359.diamond.ac.uk>
2019-05-10 14:43:03 +0100

    Added more comments

--- include/Simulation.hh
+++ include/Simulation.hh
@@ -1,3 +1,15 @@
+/*
+License info
+*/
+
+/*
+TomoSim is class used to simulate tomography experiments using the Geant4 toolkit library. This class
+is wrapped in Cython and to be controlled from Python. It manages the control of Geant4 and all other 
+classes in this library. 
+
+Author: Oliver Jack Wenman
+*/
+
 #ifndef Simulation_h
 #define Simulation_h 1
 
@@ -27,72 +39,109 @@ class G4RunManager;
 class G4VisManager;
 class G4UImanager;
 
-//Functions that end with "_pywrapped" mean they have been wrapped in Cython and can be called from python
+//Functions that end with "_pywrapped" mean they have been wrapped in Cython.
 
 class Simulation
 {
 	public:
-		Simulation();
 		Simulation(int verb, bool interactive);
 		~Simulation(); 
 
 //================================================================================================================
+/*
+        Functions that used to execute macro files which contain a list of commands that control the simulation
+        settings.
+*/
 
-		//Apply a list of macro files or commands to the simulation
+/*
+        Execute a list of macro files. Calls the applymacrofile_pywrapped(string) function in a loop.
+*/
 		void addmacros_pywrapped      (std::vector<std::string> macroFiles);
-		void applycommand_pywrapped   (std::string command);
+		
+/*
+        Reads a macro file, calls the applycommand_pywrapped(string) function when a command is detected.
+        Can also be accessed using the command "/tomosim/execute /path/to/macro/file".
+*/
 		void applymacrofile_pywrapped (std::string macro);
 		
-		//Print information about the simulation to the user. Amount of info depends on verbose setting
+/*
+        Executes a command. A return value is supplied, indicating if it was successful or not. The return type
+        corrosponds to a list of error types found in "CommandStatus.hh". If in interactive mode, the user will
+        be able to resupply the command if there was an error executing it. This function is called by
+        applymacrofile_pywrapped(string).
+*/
+		void applycommand_pywrapped   (std::string command);
+
+//================================================================================================================		
+/*
+        These functions are all used together in a single function "simulate_tomography" in the .pyx file.   
+*/
+
+/*
+        Used to print information about the settings used to the user before the simulation begins.
+*/
 		void printinfo_pywrapped(std::string filepath,
 		                         unsigned long long int totalparticles, 
 		                         int numberOfimages, 
 		                         int ndarkflatFields);
-		
-//================================================================================================================		
-	
-		//Function to run the simulation
+
+/*
+        Setup any external visualisation for the simulation 
+*/	
+		void setupvis_pywrapped(std::string path,
+		                        std::string filename);
+/*
+        simulatetomography_pywrapped is called within a loop, simulating a single set of data each time. 
+        The Cython side will then retrieve each appropriate data set and save it in a NeXus file at the 
+        end of each loop in the NXtomo format. The rotation_angle will then be changed for the next 
+        projection in the loop to simulate. 
+*/
         int run_pywrapped(unsigned long long int totalparticles, 
                           int_vector1D           imageInfo, 
                           double                 rotation_angles);
-                          
+
+//================================================================================================================		                          
+/*
+        Functions designed to be wrapped in Savu
+*/
         int runsingleprojection_pywrapped (unsigned long long int totalparticles,
                                            bool   flatfield,
                                            double rotation_angle,
                                            double zposition);    
 
+//================================================================================================================		
         //Functions to get data from detectors
 
 		//Absorption
-		int_vector1D    getAbsorption_pywrapped()    {return DC->GetAbsorptionDetector()->GetSensitiveDetector()->GetData(); }
-		double_vector1D getAbsHalf3Dim_pywrapped()   {return DC->GetAbsorptionDetector()->GetHalfDimensions();               }
-		int             getNumAbsXpixels_pywrapped() {return DC->GetAbsorptionDetector()->GetNumberOfxPixels();              }
-		int             getNumAbsYpixels_pywrapped() {return DC->GetAbsorptionDetector()->GetNumberOfyPixels();              }
+		int_vector1D    getAbsorption_pywrapped()    {return detectorManager->GetAbsorptionDetector()->GetSensitiveDetector()->GetData(); }
+		double_vector1D getAbsHalf3Dim_pywrapped()   {return detectorManager->GetAbsorptionDetector()->GetHalfDimensions();               }
+		int             getNumAbsXpixels_pywrapped() {return detectorManager->GetAbsorptionDetector()->GetNumberOfxPixels();              }
+		int             getNumAbsYpixels_pywrapped() {return detectorManager->GetAbsorptionDetector()->GetNumberOfyPixels();              }
 		
 		//Fluorescence
-		int_vector3D    getFullMappingFluore_pywrapped() {return DC->GetFluorescenceDetector()->GetSensitiveDetector()->GetFullMapping();    }
-		int_vector1D    getFullFieldFluore_pywrapped()   {return DC->GetFluorescenceDetector()->GetSensitiveDetector()->GetFullField();      }
-		double_vector1D getFluoreEneBins_pywrapped()     {return DC->GetFluorescenceDetector()->GetSensitiveDetector()->GetEnergyBins();     }
-		bool            fluorFMactive_pywrapped()        {return DC->GetFluorescenceDetector()->GetSensitiveDetector()->FullMappingActive(); }
-		bool            fluorFFactive_pywrapped()        {return DC->GetFluorescenceDetector()->GetSensitiveDetector()->FullFieldActive();   }
-		int             getNumFluorbins_pywrapped()      {return DC->GetFluorescenceDetector()->GetSensitiveDetector()->GetNumberOfBins();   }
+		int_vector3D    getFullMappingFluore_pywrapped() {return detectorManager->GetFluorescenceDetector()->GetSensitiveDetector()->GetFullMapping();    }
+		int_vector1D    getFullFieldFluore_pywrapped()   {return detectorManager->GetFluorescenceDetector()->GetSensitiveDetector()->GetFullField();      }
+		double_vector1D getFluoreEneBins_pywrapped()     {return detectorManager->GetFluorescenceDetector()->GetSensitiveDetector()->GetEnergyBins();     }
+		bool            fluorFMactive_pywrapped()        {return detectorManager->GetFluorescenceDetector()->GetSensitiveDetector()->FullMappingActive(); }
+		bool            fluorFFactive_pywrapped()        {return detectorManager->GetFluorescenceDetector()->GetSensitiveDetector()->FullFieldActive();   }
+		int             getNumFluorbins_pywrapped()      {return detectorManager->GetFluorescenceDetector()->GetSensitiveDetector()->GetNumberOfBins();   }
 		
 		//Diffraction
-		int_vector2D GetDiffractionData() {/*return data -> GetDiffractionData();*/}
+		int_vector2D GetDiffractionData() {return int_vector2D()/*data -> GetDiffractionData()*/;}
 		
 		//Beam energy
-		int_vector1D    getBeamEnergy_pywrapped()     {return PGA -> GetBeamIntensity();  }
-        double_vector1D getBeamEnergyBins_pywrapped() {return PGA -> GetBeamEnergy(); }
-        int             getbeambins_pywrapped()       {return PGA -> GetNumberOfBins();}
+		int_vector1D    getBeamEnergy_pywrapped()     {return beamManager -> GetBeamIntensity();  }
+        double_vector1D getBeamEnergyBins_pywrapped() {return beamManager -> GetBeamEnergy(); }
+        int             getbeambins_pywrapped()       {return beamManager -> GetNumberOfBins();}
 
 //================================================================================================================
 		
 		//Set pywrapped functions
-		void setSavingTime_pywrapped (double Time)                            {PGA -> SetSavingTime(Time);              }
+		void setSavingTime_pywrapped (double Time)                            {beamManager -> SetSavingTime(Time);              }
 		
 		//Free memory of data
-		void freedataMemory_pywrapped() {DC->GetAbsorptionDetector()->GetSensitiveDetector()->FreeMemory();
-		                                 DC->GetFluorescenceDetector()->GetSensitiveDetector()->FreeMemory();}	
+		void freedataMemory_pywrapped() {detectorManager->GetAbsorptionDetector()->GetSensitiveDetector()->FreeMemory();
+		                                 detectorManager->GetFluorescenceDetector()->GetSensitiveDetector()->FreeMemory();}	
 		
 		//Set functions
 		void SetSeed(long int value);    //{seedCmd = value;}
@@ -117,9 +166,9 @@ class Simulation
 		G4UImanager            *UImanager;
 		DefineMaterials        *materials;
 		SampleConstruction     *sampleconstruction;
-		DetectorConstruction   *DC;
-		PhysicsList            *PL;
-		PrimaryGeneratorAction *PGA;
+		DetectorConstruction   *detectorManager;
+		PhysicsList            *physicsManager;
+		PrimaryGeneratorAction *beamManager;
 		StackingAction         *particleManager;
 		SteppingAction         *stepManager;
 
