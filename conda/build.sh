5bb09fe5dc72051cddcec6da4adf9ab61f3714dc
Wenman <xol73553@ws359.diamond.ac.uk>
2019-06-13 09:16:28 +0100

    Updated sim to g4tomosim

--- conda/build.sh
+++ conda/build.sh
@@ -4,5 +4,5 @@ cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ${SRC_DIR}
 make
 #make install
 mkdir -p ${PREFIX}/lib/python${PY_VER}/site-packages
-cp src/sim.so ${PREFIX}/lib/python${PY_VER}/site-packages
+cp src/g4tomosim.so ${PREFIX}/lib/python${PY_VER}/site-packages
 cp ${SRC_DIR}/src/NexusFormatter.py ${PREFIX}/lib/python${PY_VER}/site-packages
